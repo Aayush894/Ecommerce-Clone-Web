17e0b27f119b9934e0c39bcf5e0c9c598b2ebc0a
Aayush Kumar Shrivastava <aayushs894@gmail.com>
2024-01-26 10:08:26 +0530

    Make Routes for app

---
+++ frontend/src/App.js
@@ -0,0 +1,23 @@
+import "./App.css";
+import Home from "./screens/Home";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Router,
+  Link,
+} from "react-router-dom";
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <Routes>
+          <Route exact path="/" element={<Home/>} />
+          
+        </Routes>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
